544f69f520e9d57ce371c1ab99248c110b28b1c4
Ritika98Saxena <73172275+Ritika98Saxena@users.noreply.github.com>
2020-11-21 13:12:05 +0530

    Add files via upload

---
+++ Add_numb.java
@@ -0,0 +1,21 @@
+package com.java.programming;
+
+public class Add_numb {
+	
+	 void add() {
+		 
+		System.out.println("This is my method");
+	}
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int sum;
+		sum= a+b;
+		System.out.println(sum);
+		Add_numb an = new Add_numb();
+		an.add();
+		
+	}
+
+}
